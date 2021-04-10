14a90045d4e095b68e6685261cd07084837e560b
Ryu jongheon <lapy@lapy.link>
2021-04-10 20:31:45 +0900

    Update EchoCommand.cs

--- Commands/EchoCommand.cs
+++ Commands/EchoCommand.cs
@@ -1,4 +1,4 @@
-#pragma warning disable CS1998 // 非同期メソッドは、'await' 演算子がないため、同期的に実行されます
+#pragma warning disable CS1998 // 非同期メソッドは、'await' 演算子がないため、同期的に実行されます / 비동기 메소드는 'await' 연산자가 없어서 동기로 실행
 using System.Threading.Tasks;
 using BotBone.Core;
 using BotBone.Core.Api;
@@ -12,7 +12,7 @@ namespace Citrine.Core
 
 		public override string Usage => "/echo <text>";
 
-		public override string Description => "引数をそのままオウム返しします。";
+		public override string Description => "인자의 문자열을 그대로 반환합니다.";
 
 		public override async Task<string> OnActivatedAsync(ICommandSender sender, Server core, IShell shell, string[] args, string body)
 		{
